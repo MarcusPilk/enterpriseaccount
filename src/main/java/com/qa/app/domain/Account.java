f99a21a492380e20daaf46fc0ffe8cba61958217
MarcusPilk <marcuspilkington@outlook.com>
2018-08-28 17:15:17 +0100

    Account Entity and local H2 db w/ import sql

---
+++ src/main/java/com/qa/app/domain/Account.java
@@ -0,0 +1,62 @@
+package com.qa.app.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "account_number") @NotNull
+    private int accountNumber;
+
+    public Account() {
+    }
+
+    public Account(String firstName, String lastName, int accountNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.accountNumber = accountNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+}
